4950a3290c0d06a5ad1578fe46aadb0530f31b55
Harinath-0704 <m.harinath0103@gmail.com>
2026-08-07 14:46:34 +0530

    Add basic arithmetic operator validation program

---
+++ Experiments/Exp 4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+
+    printf("Enter an operator: ");
+    scanf("%c", &op);
+
+    if (op == '+' || op == '-' || op == '*' || op == '/')
+        printf("Valid Arithmetic Operator\n");
+    else
+        printf("Invalid Operator\n");
+
+    return 0;
+}
